83b9272ccbcf206c0f95757a7d619c86b79f0dfc
Mendezg1 <eurocoparich@gmail.com>
2023-09-04 17:50:31 -0600

    Concat and conjun fix

--- Shunting_Yard.py
+++ Shunting_Yard.py
@@ -1,13 +1,19 @@
+def alphanum(a):
+    return a.isalpha() or a.isnumeric() or a == "ε"
+
+
 def shunting_yard(regex):
     precedence = {'*': 3, '^': 2, '+': 1}
     output = []
     operator_list = ["*", "+", "^", "(", ")"]
     operator_stack = []
     cont = -1
+    sumflag = False
+    contsum = 0
 
     for char in regex:
         cont += 1
-        if char.isalpha() or char.isnumeric() or char == "ε":
+        if alphanum(char):
             if len(operator_stack) != 0 and len(operator_stack) >= 2:
                 if operator_stack[-2] != "(":
                     if operator_stack[-1] == "*":
@@ -15,8 +21,10 @@ def shunting_yard(regex):
                     if operator_stack[-1] == "+":
                         output.append(operator_stack.pop())
             output.append(char)
+            if sumflag:
+                contsum += 1
             if len(output) >= 2:
-                if (output[-2].isalpha() or output[-2].isnumeric() or output[-2] == "ε") and len(operator_stack) == 0:
+                if (alphanum(output[-2])) and len(operator_stack) == 0:
                     output.append("^")
                 elif (output[-2] in operator_list) and len(operator_stack) == 0:
                     if regex[cont+1] == "*" :
@@ -25,16 +33,32 @@ def shunting_yard(regex):
                         break
                     else:
                         output.append("^")
+                elif operator_stack[-1] == "(" and len(operator_stack) == 1:
+                    if alphanum(output[-2]):
+                        output.append("^")
+                    elif (output[-2] in operator_list):
+                        output.append("^")
                 elif operator_stack[-1] == "+" and len(operator_stack) == 1:
                     output.append(operator_stack.pop())
+            if contsum == 2:
+                if cont < len(regex) - 1:
+                    if regex[cont+1] != ")":
+                        if operator_stack:
+                            output.append(operator_stack.pop())
+                        contsum = 0
+                        sumflag = False
         elif char in "+":
-            operator_stack.append(char) 
+            operator_stack.append(char)
+            contsum += 1 
+            sumflag == True
         elif char == "*":
             if operator_stack:
                 if operator_stack[-1] == "(":
                     output.append("*")
-            elif (output[-1].isnumeric() or output[-1].isalpha()) or ((output[-2].isnumeric() or output[-2].isalpha()) and (output[-1] == "^" or output[-1] == "+" )  ):
-                output.append("*")
+                elif (alphanum(output[-1])) or ((alphanum(output[-2])) and (output[-1] == "^" or output[-1] == "+" )):
+                    output.append("*")
+                elif output[-1] in "^+" and alphanum(output[-2]):
+                    output.append("*")
         elif char == "(":
             operator_stack.append(char)
         elif char == ")":
@@ -43,9 +67,16 @@ def shunting_yard(regex):
             if not operator_stack or operator_stack[-1] != "(":
                 raise ValueError("Error: Paréntesis no cerrado.")
             operator_stack.pop()
-
+    if output[-1] != "^":
+        a = next((x for x in range(len(output)) if output[x] == "^"), None)
+        if a and  a > 0:
+                output.append("^")
+            
         
-    
+            
+        
+
+            
     while operator_stack:
         if operator_stack[-1] == "(":
             
@@ -67,7 +98,7 @@ except ValueError as e:
     print(e)
 
 try:
-    postfix_expression = shunting_yard("(a+b*+aaa(a+b)")
+    postfix_expression = shunting_yard("(a+b*+aaa(a+b))")
     print("Expresión regular en notación postfix:", postfix_expression)
 except ValueError as e:
     print(e)
