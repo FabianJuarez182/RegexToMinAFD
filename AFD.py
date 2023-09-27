220b0f72fcfcacf772c7390b4d90a7dd95a5e747
Adrian Fulladolsa <ful21592@uvg.edu.gt>
2023-09-26 19:23:26 -0600

    AFN to AFD fixed

--- AFD.py
+++ AFD.py
@@ -13,6 +13,7 @@ class AFD:
 
 
     def generar_json_afd(self, nombre_archivo):
+        
         afd_data = {
             "ESTADOS": [list(map(int, estado)) for estado in self.afd_estados],
             "SIMBOLOS": self.simbolos,
@@ -53,20 +54,91 @@ def createDFA(filename):
         afn = nfaTemp(afn["ESTADOS"],afn["SIMBOLOS"],afn["INICIO"],afn["ACEPTACION"],afn["TRANSICIONES"])
         afd = subset_construction(afn)
         
-        afd.generar_json_afd("AFDp.py")
+        afd.generar_json_afd("AFD.json")
         
 
 def subset_construction(nfa):
     dfa_states = []  
-    dfa_start_state = epsilon_closure(nfa, nfa.start_state)
+    dfa_transitions = {}
+    dfa_start_state = epsilon_closure(nfa, [nfa.start_state])
+    dfa_aceptation = []
     
     dfa_states.append(dfa_start_state)  
 
+    for state in dfa_states:
+        tempTrans = {}
+        for simbolo in nfa.alphabet:
+            if simbolo != "ε":
+                paso = []
+                for num in state:
+                    if num in nfa.transitions:
+                        if simbolo in nfa.transitions[num]:
+                            for i in nfa.transitions[num][simbolo]:
+                                paso.append(i)
+                    if num in nfa.accept_states and state not in dfa_aceptation:
+                        dfa_aceptation.append(state)
+                        
+                if paso:
+                    tempTrans[simbolo] = epsilon_closure(nfa,paso)
+                    if tempTrans[simbolo] not in dfa_states:
+                        dfa_states.append(tempTrans[simbolo])
+                
+
+        dfa_transitions[tuple(state)] = tempTrans
+        
+        
+            
+        
+    dfa_states, dfa_transitions = checkTState(nfa, dfa_states, dfa_transitions)
+    
+    nfa.alphabet.remove("ε")
+    
+    tempTransiciones = []
+    for trans in dfa_transitions:
+        for simbolo in dfa_transitions[trans]:
+            tempTransiciones.append([trans, simbolo, dfa_transitions[trans][simbolo]])
+    dfa_transitions = tempTransiciones
+    
+    
+    return AFD(dfa_states, nfa.alphabet, dfa_transitions, dfa_start_state, dfa_aceptation)
+
 
 def epsilon_closure(nfa, state):
-    
+    stack = []
+    closure = state
+    for key in nfa.transitions:
+        if key in closure:
+            for keys in nfa.transitions[key]:
+                if keys == "ε":
+                    for elem in nfa.transitions[key][keys]:
+                        stack.append(elem)
 
+    while stack:
+        elems = stack.pop()
+        if elems not in closure:
+            closure.append(elems)
+            for key in nfa.transitions:
+                if key == elems:
+                    for keys in nfa.transitions[key]:
+                        if keys == "ε":
+                            for elem in nfa.transitions[key][keys]:
+                                stack.append(elem)
+    
     return closure
+
+def checkTState(nfa, states, transitions):
+    absolvente = [max(nfa.states) + 1]
+    for state in states:
+        for simbolo in nfa.alphabet:
+            if simbolo != "ε":
+                if tuple(state) in transitions and simbolo not in transitions[tuple(state)]:
+                    if absolvente not in states:
+                        states.append(absolvente)
+                        transitions[tuple(absolvente)] = {}
+                    transitions[tuple(state)][simbolo] = absolvente
+        transitions[tuple(state)] = dict(sorted(transitions[tuple(state)].items()))
+                
+    return states, transitions     
 
 
         
